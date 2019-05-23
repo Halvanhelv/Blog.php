285f95b5b70d8974a9d63b7d59dd704291a051b3
SynthesisOne <developerfromrussia@gmail.com>
2019-05-23 16:23:38 +0300

    Firs

---
+++ includes/config.php
@@ -0,0 +1,12 @@
+<?php
+$config = array(
+'title' => "Блог IT_Минималиста",
+'vk_url' => 'https://vk.com',
+'db' => array(
+    'server' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'name' => 'test_blog'
+)
+);
+require 'db.php';
